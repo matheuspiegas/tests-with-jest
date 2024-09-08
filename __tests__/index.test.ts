aaedef2b85619ec6f77aa9a8f7d782de205c148e
matheuspiegas <matheusaugustopiegassales@gmail.com>
2024-09-08 10:21:30 -0300

    08/09/2024 10:21

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,6 +2,7 @@ beforeAll(() => {
 	jest.deepUnmock("../src/db");
 });
 import { server } from "../src";
+import prisma from "../src/db";
 
 describe("Integration tests", () => {
 	describe("GET route", () => {
@@ -10,95 +11,95 @@ describe("Integration tests", () => {
 				method: "GET",
 				url: "/api/user/read",
 			});
+			expect(response.statusCode).toBe(200);
+		});
+	});
+
+	describe("POST route", () => {
+		it("should return 201", async () => {
+			const response = await server.inject({
+				method: "POST",
+				url: "/api/user/create",
+				payload: { title: "Test todo" },
+			});
+			expect(response.statusCode).toBe(201);
+		});
+		it("should return 400 with an empty title property in body", async () => {
+			const response = await server.inject({
+				method: "POST",
+				url: "/api/user/create",
+				payload: { title: "" },
+			});
+			expect(response.statusCode).toBe(400);
+		});
+	});
+
+	describe("PUT route", () => {
+		it("should return 400 if missing title", async () => {
+			const response = await server.inject({
+				method: "PUT",
+				body: { title: "" },
+				url: "/api/user/update/5",
+			});
+			expect(response.statusCode).toBe(400);
+		});
+		it("should return 400 if missing id", async () => {
+			const response = await server.inject({
+				method: "PUT",
+				body: { title: "title" },
+				url: "/api/user/update/",
+			});
+			expect(response.statusCode).toBe(400);
+		});
+		it("should return 404 if todo id not found", async () => {
+			const response = await server.inject({
+				method: "PUT",
+				payload: { title: "test" },
+				url: "/api/user/update/1",
+			});
+			expect(response.statusCode).toBe(404);
+		});
+		it("should return 200 if todo updated", async () => {
+			const response = await server.inject({
+				method: "PUT",
+				payload: { title: "updated title" },
+				url: "/api/user/update/5",
+			});
 			console.log(response.json());
 			expect(response.statusCode).toBe(200);
 		});
 	});
 
-	// describe("POST route", () => {
-	// 	it("should return 201", async () => {
-	// 		const response = await server.inject({
-	// 			method: "POST",
-	// 			url: "/api/user/create",
-	// 			payload: { title: "Test todo" },
-	// 		});
-	// 		console.log(response);
-	// 		// expect(response.statusCode).toBe(201);
-	// 	});
-	// 	it("should return 400 with an empty title property in body", async () => {
-	// 		const response = await server.inject({
-	// 			method: "POST",
-	// 			url: "/api/user/create",
-	// 			payload: { title: "" },
-	// 		});
-	// 		expect(response.statusCode).toBe(400);
-	// 	});
-	// });
+	describe("DELETE route", () => {
+		beforeAll(async () => {
+			await prisma.todos.create({
+				data: { id: 4, title: "Test todo" },
+			});
+		});
 
-	// describe("PUT route", () => {
-	// 	it("should return 400 if missing title", async () => {
-	// 		const response = await server.inject({
-	// 			method: "PUT",
-	// 			body: { title: "" },
-	// 			url: "/api/user/update/2",
-	// 		});
-	// 		expect(response.statusCode).toBe(400);
-	// 	});
-	// 	it("should return 400 if missing id", async () => {
-	// 		const response = await server.inject({
-	// 			method: "PUT",
-	// 			body: { title: "title" },
-	// 			url: "/api/user/update/",
-	// 		});
-	// 		expect(response.statusCode).toBe(400);
-	// 	});
-	// 	it("should return 404 if todo id not found", async () => {
-	// 		const response = await server.inject({
-	// 			method: "PUT",
-	// 			payload: { title: "test" },
-	// 			url: "/api/user/update/1",
-	// 		});
-	// 		expect(response.statusCode).toBe(404);
-	// 	});
-	// 	// it("should return 200 if todo updated", async () => {
-	// 	// 	const response = await server.inject({
-	// 	// 		method: "PUT",
-	// 	// 		payload: { title: "updated title" },
-	// 	// 		url: "/api/user/update/5",
-	// 	// 	});
-	// 	// 	console.log(response.json());
-	// 	// 	expect(response.statusCode).toBe(200);
-	// 	// });
-	// });
-
-	// describe("DELETE route", () => {
-	// 	it("should return 400 if missing id", async () => {
-	// 		const response = await server.inject({
-	// 			method: "DELETE",
-	// 			url: "/api/user/delete/",
-	// 		});
-	// 		expect(response.statusCode).toBe(400);
-	// 	});
-	// 	it("should return 404 if todo id not found", async () => {
-	// 		const response = await server.inject({
-	// 			method: "DELETE",
-	// 			url: "/api/user/delete/0219",
-	// 		});
-	// 		expect(response.statusCode).toBe(404);
-	// 	});
-	// 	it("should return 200 and an object with message property if todo deleted", async () => {
-	// 		const response = await server.inject({
-	// 			method: "DELETE",
-	// 			payload: { title: "updated title" },
-	// 			url: "/api/user/delete/3",
-	// 		});
-	// 		// const parseResponse = JSON.parse(response.body);
-	// 		// expect(response.statusCode).toBe(200);
-	// 		// expect(parseResponse).toEqual(
-	// 		// 	expect.objectContaining({
-	// 		// 		message: expect.stringContaining("Todo deleted"),
-	// 		// 	}),
-	// 		// );
-	// 	});
-	// });
+		it("should return 400 if missing id", async () => {
+			const response = await server.inject({
+				method: "DELETE",
+				url: "/api/user/delete/",
+			});
+			expect(response.statusCode).toBe(400);
+		});
+		it("should return 404 if todo id not found", async () => {
+			const response = await server.inject({
+				method: "DELETE",
+				url: "/api/user/delete/219",
+			});
+			expect(response.statusCode).toBe(404);
+		});
+		it("should return 200 and todo if todo deleted", async () => {
+			const response = await server.inject({
+				method: "DELETE",
+				url: "/api/user/delete/4",
+			});
+			const parseResponse = JSON.parse(response.body);
+			expect(response.statusCode).toBe(200);
+			expect(parseResponse.id).toBe(4);
+			expect(parseResponse.title).toBe("Test todo");
+		});
+	});
 });
