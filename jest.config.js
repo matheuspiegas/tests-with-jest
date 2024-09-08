7525d1f0bb7399d4d3b8d2dd1d1bb60853d038b9
matheuspiegas <matheusaugustopiegassales@gmail.com>
2024-09-08 14:48:57 -0300

    Update jest.config.js to fix file extension regex

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
+export default {
 	testEnvironment: "node",
 	transform: {
-		"^.+.tsx?$": ["ts-jest", {}],
+		"^.+\\.tsx?$": ["ts-jest", {}],
 	},
 	setupFilesAfterEnv: ["<rootDir>/singleton.ts"],
 };
